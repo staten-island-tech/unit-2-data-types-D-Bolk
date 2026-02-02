aedd6e1d6ec5fce24807676a4a3f0fad0380827f
D-Bolk <danielb643@nycstudents.net>
2026-02-02 09:25:41 -0500

    Update app.ppy.py

--- app.ppy.py
+++ app.ppy.py
@@ -2,10 +2,21 @@
 y = float(3)
 print(x,y)
  """
-#list
 
-values = [1,2.23,5,7,2,30,15]
+
+
+""" values = [1,2.23,5,7,2,30,15]
 print(values)
 for i in values:
     print(i)
-print(values[0])
+print(values[0]) """
+
+
+
+
+number = input("Is this number even or odd")
+if int(number) == 1: 
+    print("Odd")
+else:
+    print("Even")
+
